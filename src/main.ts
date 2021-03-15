d4f5e1cad76870e88d86a3fdd42e978f33e9c221
Filipe Abrantes <filipenabrantes@gmail.com>
2021-03-15 09:31:23 -0300

    adding swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { cssSwagger } from './css-swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('API 2 - inGaia Challenge')
+    .setDescription('Calculate the property value')
+    .setVersion('1.0')
+    .setLicense(
+      'Developed by Filipe Abrantes',
+      'https://github.com/filipenabrantes',
+    )
+    .build();
+  const customOptions: SwaggerCustomOptions = {
+    customSiteTitle: 'inGaia Challenge',
+    customfavIcon: cssSwagger,
+    customCss: cssSwagger,
+  };
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, customOptions);
+  await app.listen(process.env.PORT || 3000);
+  Logger.log(`🚀 Running on ${process.env.PORT || 3000} port.`);
 }
 bootstrap();
